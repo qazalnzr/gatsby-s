e72aa1afb2f4b9fb26b79169725d90bdc0d14bf6
qazalnzr <77636202+qazalnzr@users.noreply.github.com>
2021-07-29 17:08:48 +0430

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Ghazal website'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Ghazal website'} />
       </Helmet>
       <App />
     </>
